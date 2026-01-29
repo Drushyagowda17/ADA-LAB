e288f5aaa0d183bc6cbf5e148c6d4f6c793e4392
Drushyagowda17 <drushyagmgowda@gmail.com>
2026-01-29 10:54:40 +0530

    Update insertionsort.py

--- insertionsort.py
+++ insertionsort.py
@@ -1,27 +1,3 @@
-# import time
-
-# def insertion_sort(arr):
-#     n = len(arr)
-
-#     for i in range(1, n):
-#         start_time = time.time()
-
-#         j = i - 1
-#         v = arr[i]
-#         while j >= 0 and arr[j] > v:
-#             arr[j + 1] = arr[j]
-#             j -= 1
-#         arr[j + 1] = v
-
-#         end_time = time.time()
-#         print("Time taken for element", v, ":", end_time - start_time, "seconds")
-
-#     print("Sorted array:", arr)
-
-
-# arr = list(map(int, input("Enter the elements: ").split()))
-# insertion_sort(arr)
-
 import time
 import matplotlib.pyplot as plt
 
@@ -59,3 +35,4 @@ def insertion_sort(arr):
 
 arr = list(map(int, input("Enter the elements: ").split()))
 insertion_sort(arr)
+
